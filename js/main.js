cf14d9f1bc14983db039206cfaf343a69644434c
dalia-za <dalia134731@bue.edu.eg>
2019-08-15 18:52:33 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	// body...
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+
+
+	})
